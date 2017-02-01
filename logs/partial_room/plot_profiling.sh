fd7278b15d0dae855dabb24f147f02409a6cf70d
Camilo Gomez <cagomeznu@gmai.com>
2017-02-01 18:57:49 -0300

    adds new plots for profile and fitting for times

--- logs/partial_room/plot_profiling.sh
+++ logs/partial_room/plot_profiling.sh
@@ -1,30 +1,23 @@
 gnuplot -persist <<-EOFMarker
 	set datafile separator ";"
 
+	set terminal pngcairo enhanced font "arial,18" fontscale 1.0 size 1920,1080
+	set size ratio 1 1
+
 	set title 'Code Profiling with some participants sending (Room of 28)'
 	set xlabel "Number of Messages"
 	set ylabel '% of total time'
 	set key invert reverse Left outside
-
-	set grid y
-	set grid x
 
 	set xrange[-3:30]
 	set yrange [0:100]
 	set xtics 1
 
 	set style data histograms
-	set bar 1.000000 front
-	set boxwidth 1 absolute
-	set style fill   solid 1.00 border lt -1
-	set style circle radius graph 0.02, first 0.00000, 0.00000 
-	set style ellipse size graph 0.05, 0.03, first 0.00000 angle 0 units xy
+	
+	set style fill solid 1.00 border lt -1
 	set key outside right top vertical Left reverse noenhanced autotitle columnhead nobox
-	set key invert samplen 4 spacing 1 width 0 height 0 
 	set style histogram rowstacked title textcolor lt -1
-	set style textbox transparent margins  1.0,  1.0 border
-
-	set xtics border in scale 0,0 nomirror rotate by -45  autojustify
 
 	plot 'averages.csv' u 5:xtic(1), for [i=6:21] '' u i t column(i)
 EOFMarker
